83ca7be0b12cd9e54aaf8069e4d8be3833935080
Shankar Thapa <shankasthapa@gmail.com>
2024-06-26 15:03:15 +0530

    http socket impl

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,4 @@
 "use client";
 import { io } from "socket.io-client";
-const ENDPOINT = process.env.NEXT_PUBLIC_APP_SOCKET || 'ws://169.38.137.104:9000';
+const ENDPOINT = process.env.NEXT_PUBLIC_APP_SOCKET || 'http://169.38.137.104:9000';
 export const socket = io(ENDPOINT, { transports: ['websocket'] }).connect();
